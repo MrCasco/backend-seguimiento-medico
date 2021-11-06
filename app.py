37bab886206587cccb54ba686397fdc99156d987
MrCasco <ernesto.casco.velazquez@hotmail.com>
2021-11-05 19:12:08 -0600

    Finished GET, POST, DELETE for medic

--- app.py
+++ app.py
@@ -2,8 +2,50 @@ from flask import Flask, redirect, url_for, request
 from database import db
 app = Flask(__name__)
 
+def getMedic(mail):
+    doc = db.collection('medicos').document(mail).get()
+    if doc.exists:
+        return doc.to_dict()
+    return False
+
+def insertMedic(data):
+    try:
+        mail = data['correo']
+        data.pop('correo')
+        db.collection('medicos').document(mail).set(data)
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+def deleteMedic(mail):
+    try:
+        db.collection('medicos').document(mail).delete()
+    except Exception as e:
+        return e
+    return 'Borrado correctamente'
+
 @app.route('/medic/', methods = ['POST', 'GET', 'DELETE'])
 def medic():
+    if request.method == 'POST':
+        mail = request.form['correo']
+        medic = getMedic(mail)
+        if medic:
+            return 'Medico ya registrado'
+        elif insertMedic(dict(request.form)):
+            return 'Medico guardado correctamente'
+        return 'Hubo un problema'
+    elif request.method == 'GET':
+        medic = getMedic(request.args.get('correo'))
+        if not medic:
+            return 'No existe el medico'
+        return medic
+    elif request.method == 'DELETE':
+        mail = request.form['correo']
+        return deleteMedic(mail)
+
+@app.route('/patient/', methods = ['POST', 'GET', 'DELETE'])
+def patient():
     if request.method == 'POST':
        print(dict(request.form))
        return request.form.get('nombre')
@@ -17,7 +59,7 @@ def medic():
 @app.route('/login', methods = ['POST'])
 def login():
    if request.method == 'POST':
-      user = request.form['nm']
+      user = request.form.get('correo')
       return redirect(url_for('success',name = user))
    else:
       user = request.args.get('nm')
