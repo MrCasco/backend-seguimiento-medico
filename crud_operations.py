4d76bbef9c59ae67045f1de22d96197e8e4c88a8
MrCasco <ernesto.casco.velazquez@hotmail.com>
2021-11-06 12:25:13 -0600

    Created class for prescription

--- crud_operations.py
+++ crud_operations.py
@@ -1,5 +1,35 @@
 from database import db
 
+class Prescription:
+    def getPrescription(id):
+        doc = db.collection('pacientes').document(id).get()
+        if doc.exists:
+            return doc.to_dict()
+        return False
+
+    def insertPrescription(data):
+        try:
+            patient = data['paciente']
+            medic = data['medico']
+            if Medic.getMedic(medic):
+                if Patient.getPatient(patient):
+                    db.collection('recetas').document().set(data)
+                else:
+                    return 'Paciente no valido'
+            else:
+                return 'Medico no valido'
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    def deletePrescription(id):
+        try:
+            db.collection('recetas').document(id).delete()
+        except Exception as e:
+            return e
+        return 'Baja de paciente correcta'
+
 class Patient:
     def getPatient(mail):
         doc = db.collection('pacientes').document(mail).get()
@@ -25,6 +55,8 @@ class Patient:
         return 'Baja de paciente correcta'
 
 class Medic:
+    def getPatients():
+
     def getMedic(mail):
         doc = db.collection('medicos').document(mail).get()
         if doc.exists:
